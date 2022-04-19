6119da65b0c6caf7b8f64ae2eae9dd2ecd91f867
vickyumsaenii <vicky.umsaenii@gmail.com>
2022-04-20 04:36:01 +0700

    Tugas_Latihan 1-2

---
+++ Variable_TipeData.js
@@ -0,0 +1,25 @@
+/**
+ * STUDY CASEs
+1. Menghitung volume setengah bola dengan rumus:
+
+ * -Algoritma
+ * 1. Buat variabel r = ""
+ * 2. Buat var pi = ""
+ * 3. Isi var pi = 3.14
+ * 4. Buat var volume = ""
+ * 5. Isi var volume dengan rumus (pi kali r kali r kali r)
+ * 6. cetak volume
+ * 
+ * PSEUDOCODE
+ * STORE r WITH ANY NUMBER
+ * STORE pi WITH 3.14
+ * 
+ * SET volume WITH pi MULTIPLY r MULTIPLY r MULTIPLY r DIVIDE 2)
+ * DISPLAY volume
+ */
+
+var r = 4;
+var phi = 3.14;
+
+var volume = phi * r * r * r;
+console.log(volume);
