311a61afbe62fca9ad591d490a26e5530db8327a
vickyumsaenii <vicky.umsaenii@gmail.com>
2022-04-20 19:46:44 +0700

    tugas conditional 1-3

---
+++ conditional1_3.js
@@ -0,0 +1,61 @@
+/*
+ * STUDY CASEs 1
+ * Klasifikasi level untuk karakter dalam sebuah game terdiri dari detail berikut::
+
+ * -Algoritma
+ * Klasifikasi level untuk karakter dari sebuah game
+ *
+ * Jika Power lebih dari 100, maka Status disebut "Expert"
+ * Jika Power di atas 70 sampai 100, maka Status disebut "Pro"
+ * Jika Power di atas 50 sampai 70, maka Status disebut "Normal"
+ * Jika Power di atas 30 sampai 50, maka Status disebut "Basic"
+ * Jika Power di bawah 30, maka Status disebut "Beginner"
+ * 
+ * PSEUDOCODE
+ * STORE Power WITH ANY NUMBER
+ *
+ * * power lebih besar dari 100 : "Expert"
+ * power di atas 70 - 100 : "Pro"
+ * power diatas 50 -70 : "Normal"
+ * power diatas 30 -50 : "Basic"
+ * power di bawah 30 : "Baginer"
+ * END IF
+ */
+
+// var Power = 500;
+// if (Power > 100) {
+//   console.log("Expert Player");
+// } else if (Power > 70 && Power <= 100) {
+//   console.log("Pro Player");
+// } else if (Power > 50 && Power <= 70) {
+//   console.log("Normal Player");
+// } else if (Power > 30 && Power <= 50) {
+//   console.log("Basic Player");
+// } else if (Power > 30) {
+//   console.log("Beginner Player");
+// }
+
+
+/*
+ * STUDY CASEs 2
+ * Klasifikasi level untuk karakter dalam sebuah game terdiri dari detail berikut::
+
+ * -Algoritma
+ * Klasifikasi level untuk karakter dari sebuah game
+ *
+ * Jika Power lebih dari 100, maka Status disebut "Expert"
+ * Jika Power di atas 70 sampai 100, maka Status disebut "Pro"
+ * Jika Power di atas 50 sampai 70, maka Status disebut "Normal"
+ * Jika Power di atas 30 sampai 50, maka Status disebut "Basic"
+ * Jika Power di bawah 30, maka Status disebut "Beginner"
+ * 
+ * PSEUDOCODE
+ * STORE Power WITH ANY NUMBER
+ *
+ * * power lebih besar dari 100 : "Expert"
+ * power di atas 70 - 100 : "Pro"
+ * power diatas 50 -70 : "Normal"
+ * power diatas 30 -50 : "Basic"
+ * power di bawah 30 : "Baginer"
+ * END IF
+ */
