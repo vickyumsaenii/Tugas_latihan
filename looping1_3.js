a67ef53b68255079e3c5906e6563cc38d88fbbfe
vickyumsaenii <vicky.umsaenii@gmail.com>
2022-04-23 10:04:08 +0700

    looping 1-3

---
+++ looping1_3.js
@@ -0,0 +1,60 @@
+// // soal 1 ;faktor
+
+// // bilang prima ;bilangan yg hanya memoliki 2 faktor
+
+// var number = ;
+// var faktor = ;
+
+// for (var i = 1; i <= number; i++) {
+//   if (number % i === 0) {
+//     // console.log(i)
+//     faktor++:
+//   }
+// }
+// // console.log(Faktor: " + faktor");
+// if (faktor === 2){
+//   console.log(number + "adalah Bilangan Prima")
+// }) else{
+//   console.log(number + "adalah Bukan Bilangan Prima")
+// }
+
+//soal 2
+// var number = 5
+// var temp = "";
+
+// for (var i = 0; i < number; i++) {
+//   for (var j = 0; j < i + 1; j++){
+//     if(j % 2 === 0){
+//       temp += "#"
+//     } else {
+//       temp += "x"  
+//     }
+// }
+// console.log(temp)
+// temp =""
+// }
+// console.log(temp)
+
+
+// soal 3
+
+var number = 5;
+var temp = '';
+
+for (var i = 0; i < number; i++) {
+  for (var j = 0; j < number; j++) {
+    if (i === 0 || i === number - 1) {
+      temp += "#"
+    }else{
+      if (j === 0 || j === number - 1) {
+        temp += "#"
+      }else {
+        temp += ""
+      }
+    }
+  }
+  console.log(temp)
+  temp= ''
+}
+
+
