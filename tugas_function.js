d897015540cad1b828b019b515912ed7d08edae0
vickyumsaenii <vicky.umsaenii@gmail.com>
2022-04-24 10:38:38 +0700

    tugas_function

--- tugas_function.js
+++ tugas_function.js
@@ -1,29 +1,65 @@
 // 1 
 
-function checkPrime(n) {
-  var faktor = 0;
+// function checkPrime(n) {
+//   var faktor = 0;
 
-  for (var i = 1; i <= n; i++) {
-    if (n % i === 0) {
-      faktor
-    }
-  }
+//   for (var i = 1; i <= n; i++) {
+//     if (n % i === 0) {
+//       faktor
+//     }
+//   }
 
-  // if (faktor === 2)
-  return faktor === 2 ? true : false;
-}
+//   // if (faktor === 2)
+//   return faktor === 2 ? true : false;
+// }
 
-function printPrimes(num) {
-  // code here
-  var i = 1;
-  var x = 0;
-  while (x <num) {
-    if (checkPrime(i) === true) {
-      console.log(i)
-      x++
-    }
-    i++
-  }
+// function printPrimes(num) {
+//   // code here
+//   var i = 1;
+//   var x = 0;
+//   while (x <num) {
+//     if (checkPrime(i) === true) {
+//       console.log(i)
+//       x++
+//     }
+//     i++
+//   }
   
-}
-printPrimes(10)
+// }
+//printPrimes(10)
+
+// // 2
+// function triple(a, b, c) {
+//   var resultA = a ** 2
+//   var resultB = b ** 2
+//   var resultc = c ** 2
+
+//   return resultA + resultB === resultc ? true : false
+  
+// }
+// console.log(triple)
+
+
+
+// 3
+
+// function splitToArray(numStr) {
+//   var temp + ""
+//   var temp = [];
+
+//   for (var i = 0; i < numStr.lengh; i ++) {
+//     if (i !== numStr.lengh - 1) {
+//       temp += numStr[i] + numStr[i + 1]
+//       tumpNum.push(Number(temp))
+ 
+//     }
+//   }
+//   return tempNum
+// }
+
+// function findTheelargest(numStr) {
+
+//   if (numStrc === undefined||)
+  
+// }
+
