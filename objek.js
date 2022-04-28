c75107f19d3d0997f83e2283c08cd74c368045c5
vickyumsaenii <vicky.umsaenii@gmail.com>
2022-04-29 06:12:40 +0700

    objek

---
+++ objek.js
@@ -0,0 +1,12 @@
+var student = {
+  name : "Vincent",
+  gpa : 3.21,
+  isGraduated : true,
+  "able to code": "yes",
+  100 : 1
+// }
+// console.log(student.name)
+// console.log(student.gpa)
+// console.log(student["isGraduated"])
+// console.log(student["able to code "])
+// console.log(student["100"])
